e96bd19acdb81f184d0b8f3a7df0d165c4772e92
Nitin kumar <nitinkrc2432@gmail.com>
2025-06-22 01:02:02 +0530

    Add files via upload

---
+++ another method for 1&0 triangle.c
@@ -0,0 +1,50 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter no of rows :");
+    scanf("%d",&n);
+    // int a=1;
+    //printf("Enter no of columns :");
+    //scanf("%d",&m);
+    for(int i=1;i<=n;i++){
+        // if(i%2!=0) a=1;
+        // else a=0;
+        
+        for(int j=1;j<=i;j++){
+            // printf("%d",a);
+             if((i+j)%2==0) printf("1");
+             else printf("0");
+            
+    
+            
+            
+            
+            
+            
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
