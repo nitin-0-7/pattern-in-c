3bd0d2f518ed6749861cc3b3f28c3472fc960fc4
Nitin kumar <nitinkrc2432@gmail.com>
2025-06-22 14:54:43 +0530

    Add files via upload

---
+++ opposite triangle.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter no of lines :" ); 
+     scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+            for (int k=1;k<=i;k++){
+             printf("*");
+        }
+        printf("\n");
+        
+    }
+        
+            
+       
+
+
+    return 0;
+}
