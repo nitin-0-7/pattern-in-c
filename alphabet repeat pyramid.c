bebaed15cd53c5b4a9c48e3f27ef5dbf168e25a9
Nitin kumar <nitinkrc2432@gmail.com>
2025-06-24 00:20:17 +0530

    Add files via upload

---
+++ alphabet repeat pyramid.c
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter no of lines ");
+    scanf("%d",&n);
+    for( int i=1;i<=n;i++){
+        int a=i-1;
+        
+        
+        for( int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int q=1;q<=i;q++){
+            char ch=(char)q+64;
+            
+         printf("%c",ch);
+        }
+        for(int k=1;k<=i-1;k++){
+            char ch=(char)a+64;
+            printf("%c",ch);
+            a--;
+        }
+        printf("\n");
+        
+    }
+
+    return 0;
+    
+}
