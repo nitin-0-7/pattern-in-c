67f0298e18885cb4506d5d07594178963a47fa43
Nitin kumar <nitinkrc2432@gmail.com>
2025-06-28 13:50:16 +0530

    Add files via upload

---
+++ hollow pyramid bounded with alphabets.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter no of lines ");
+    scanf("%d",&n);
+    int nsp=1;
+    int nst=n;
+    for( int i=1;i<=2*n+1;i++){
+        printf("%c",i+64);
+    }
+    printf("\n");
+    for(int i=1;i<=n;i++){
+        int a=1;
+        for(int j=1;j<=nst;j++){
+            printf("%c",a+64);
+            a++;
+        }
+        for(int k=1;k<=nsp;k++){
+            printf(" ");
+            a++;
+        }
+        for(int j=1;j<=nst;j++){
+            printf("%c",a+64);
+            a++;
+        }
+        nsp+=2;
+        nst--;
+        printf("\n");
+        
+    }
+
+    return 0;
+}
