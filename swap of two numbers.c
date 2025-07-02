5895c96799a6caa20375bb38f522347d4d8637f4
Nitin kumar <nitinkrc2432@gmail.com>
2025-07-02 12:05:35 +0530

    Add files via upload

---
+++ swap of two numbers.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    
+    int m;
+    printf("Enter m:");
+    scanf("%d",&m);
+    
+    int temp;
+    temp=n;
+    n=m;
+    m=temp;
+    printf("The value of n is %d\n",n);
+    printf("The value of m is %d",m);
+    return 0;
+}
